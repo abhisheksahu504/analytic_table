d8a17885f779306cd8e5d34b8d929cb6bc788bba
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-06-26 14:59:04 +0530

    json added and mantime framework added

---
+++ src/List.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Table } from "@mantine/core";
+import datas from "./Manufac _ India Agro Dataset.json";
+export const List = () => {
+  return <div>List</div>;
+};
